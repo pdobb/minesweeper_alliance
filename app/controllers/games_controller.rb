0b4463216fc81d127173e4dad72ad46de7c5bc2b
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-08-15 23:12:51 -0500

    Use View Models for Game, Board, & Cell

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,14 +4,53 @@ class GamesController < ApplicationController
   before_action :require_game, only: :show
 
   def show
-    @board = @game.board
   end
 
   private
 
   def require_game
-    @game =
+    game =
       Game.for_status_in_progress.take ||
         Game.create_for(:beginner)
+
+    @game_view = GameView.new(game)
+  end
+
+  # GamesController::GameView is a view model for displaying our Game Model.
+  class GameView
+    attr_reader :object
+
+    def initialize(object)
+      @object = object
+    end
+
+    def rows
+      board.cells_grid.to_a.map { |row|
+        row.map { |cell| CellView.new(cell) }
+      }
+    end
+
+    private
+
+    def board
+      object.board
+    end
+  end
+
+  # GamesController::CellView
+  class CellView
+    attr_reader :object
+
+    def initialize(object)
+      @object = object
+    end
+
+    def css_background_color
+      "bg-slate-300"
+    end
+
+    def render
+      object.render
+    end
   end
 end
