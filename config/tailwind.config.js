383457bf7263911b51d9fd2fa7149a3c116d2f05
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-08-24 02:17:54 -0500

    Fix tailwind config; missing Cell bg-color classes
    Now that we've moved our View models out of the controllers folder and
    into the views folder, we need to reconfigure Tailwindcss to look in
    `"./app/views/**/*.rb"` -- another non-standard location.
    - While I was editing the config, I removed the `haml` and `slim`
      extensions as well since we'll never need them.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,8 +5,7 @@ module.exports = {
     "./public/*.html",
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
-    "./app/views/**/*.{erb,haml,html,slim}",
-    "./app/controllers/**/*.rb",
+    "./app/views/**/*.{erb,html,rb}",
   ],
   theme: {
     extend: {
