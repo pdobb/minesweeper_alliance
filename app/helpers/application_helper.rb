b70343dae5850408cded4b231db63a6b4eec43a7
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-08-30 23:23:01 -0500

    Indicate which is the the Currently Active Nav Link
    This uses one of my long-time helper methods for adding an `active` CSS
    class to the currently active main Nav Links.
    
    Then we just style what it means to be an `a.active` with CSS.
    
    Also: Add a bottom border to the header section for visual break-up
    from the rest of the page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,36 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # #active_link_to is a wrapper around Rails's `link_to` helper that adds
+  # the given `active_css_class` (default: "active") to the `link_to`'s CSS
+  # class "list" if either:
+  #  - The given `url` is the same as the current page's, or
+  #  - The current `controller_name` is contained in the given `includes` list.
+  # Then it just forwards the rest on to `link_to` as per usual.
+  def active_link_to(
+        name = nil,
+        url = nil,
+        active_css_class: "active",
+        includes: [],
+        **options,
+        &)
+    url = name if block_given?
+
+    options[:class] = [
+      options[:class],
+      { active_css_class => active_link?(url, includes:) },
+    ]
+
+    if block_given?
+      link_to(url, **options, &)
+    else
+      link_to(name, url, **options)
+    end
+  end
+
+  private
+
+  def active_link?(url, includes:)
+    current_page?(url) || includes.include?(controller_name)
+  end
 end
