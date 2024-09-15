77dbd649dfb28ece640d4dab6d7067682e2f4f1b
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-09-15 15:21:43 -0500

    Add/improve .console methods for ConsoleBehaviors objects
    Use `MyObject.cons[ole](:<class_method_name>)` to simplify conversion of
    the collection results to *::Console objects. Example:
    
    ```ruby
    Cell.cons("is_not_flagged.is_not_revealed")
      # => [<Cell[2](◻️) (1, 0) :: nil>, ...]
    ```
    
    And, while I was at it, I went ahead and added the same convenience to
    the existing #console method. If no arguments are given, then it does
    the same thing as before. If any arguments are given, however, then it
    processes the method (chain) and wraps the result in *::Console objects.
    Huzzah!
    
    See comments for more usage notes.
    
    Note:
    - Since this is a Console object behavior, I've gone ahead and allowed
      sending of private messages via the `cons[ole]` methods.
    - If a result comes back that doesn't respond to `console`, we just
      return the result instead.
    - If a single-item Array comes back, we pop the item out of the Array,
      for your convenience.

--- lib/console_behaviors.rb
+++ lib/console_behaviors.rb
@@ -5,6 +5,80 @@
 #
 # @see ConsoleObjectBehaviors
 module ConsoleBehaviors
-  def console = self.class::Console.new(self)
+  extend ActiveSupport::Concern
+
+  included do
+    private_class_method :call_method_chain
+  end
+
+  class_methods do
+    # :reek:ManualDispatch
+    # :reek:TooManyStatements
+
+    # @example Given: A single method.
+    #   Cell.cons("all")
+    #   # => [<Cell[2](◻️) (1, 0) :: nil>, ...]
+    #
+    # @example Given: A method chain.
+    #   Cell.cons("is_not_flagged.is_not_revealed")
+    #   # => [<Cell[2](◻️) (1, 0) :: nil>, ...]
+    #
+    #   Cell.cons(%i[is_not_flagged is_not_revealed])
+    #   # => [<Cell[2](◻️) (1, 0) :: nil>, ...]
+    #
+    #   Cell.cons("first")
+    #   # => <Cell[2](◻️) (0, 0) :: nil>
+    def console(method_chain, context: self)
+      result = call_method_chain(method_chain, context:)
+      array =
+        Array(result).map { |object|
+          object.respond_to?(:console) ? object.console : object
+        }
+      array.one? ? array.first : array
+    end
+    alias_method :cons, :console
+
+    def call_method_chain(method_chain, context: self)
+      methods =
+        case method_chain
+        when String, Symbol then method_chain.to_s.split(".")
+        else Array.wrap(method_chain)
+        end
+
+      methods.inject(context) { |object, method_name|
+        object.__send__(method_name)
+      }
+    end
+  end
+
+  # @example
+  #   Cell.first.cons
+  #   # => <Cell[1](◻️ / 💣) (0, 0) :: nil>
+  #
+  # @example Given: A single method.
+  #   Cell.first.cons("neighbors")
+  #   # => [<Cell[2](◻️) (1, 0) :: nil>, ...]
+  #
+  # @example Given: A method chain.
+  #   Cell.first.cons("neighbors.is_not_revealed")
+  #   # => [<Cell[2](◻️) (1, 0) :: nil>, ...]
+  #
+  #   Cell.first.cons(%i[neighbors is_not_revealed])
+  #   # => [<Cell[2](◻️) (1, 0) :: nil>, ...]
+  #
+  #   Cell.first.cons("neighbors.is_not_revealed.first")
+  #   # => <Cell[2](◻️) (1, 0) :: nil>
+  #
+  # @example Given: A private method.
+  #   Cell.first.cons("determine_revealed_value")
+  #   # => "💣"
+  def console(method_chain = nil)
+    if method_chain
+      self.class.console(method_chain, context: self)
+    else
+      self.class::Console.new(self)
+    end
+  end
+
   alias_method :cons, :console
 end
