0d8fd7cc7d4e3f6a73debb309dcacf0f1beed5b1
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-10-01 16:29:27 -0500

    Tidy up Welcome Banner view models/interaction
    My Home::Show::WelcomeBanner abstraction was bleeding! (What should have
    been its internals were all over the place.)

--- app/views/home/show.rb
+++ app/views/home/show.rb
@@ -2,21 +2,14 @@
 
 # Home::Show is a View Model for displaying the Home Page.
 class Home::Show
-  WELCOME_BANNER_NAME = "welcome_banner"
-  BANNER_DISMISSAL_VALUE = "dismissed"
-
   def initialize(current_game:)
     @current_game = current_game
   end
 
   def nav = Nav.new(current_game:)
 
-  def show_welcome_banner?(context:)
-    build_welcome_banner.show_welcome_banner?(context:)
-  end
-
   def welcome_banner(context:)
-    build_welcome_banner.welcome_banner(context:)
+    WelcomeBanner.new(context:)
   end
 
   def current_game? = !!current_game
@@ -40,10 +33,6 @@ class Home::Show
   private
 
   attr_reader :current_game
-
-  def build_welcome_banner
-    @build_welcome_banner ||= WelcomeBanner.new
-  end
 
   def current_game_show_view
     Games::Show.new(game: current_game)
@@ -80,28 +69,44 @@ class Home::Show
     attr_reader :current_game
   end
 
-  # Home::Show::WelcomeBanner
+  # Home::Show::WelcomeBanner is a View Model that aids in building a
+  # {Application::PermanentlyDismissableBanner} based on the given context.
   class WelcomeBanner
-    def show_welcome_banner?(context:)
+    WELCOME_BANNER_NAME = "welcome_banner"
+    BANNER_DISMISSAL_VALUE = "dismissed"
+
+    def initialize(context:)
+      @context = BannerContext.new(context:)
+    end
+
+    def show?
       context.cookies[WELCOME_BANNER_NAME] != BANNER_DISMISSAL_VALUE
     end
 
-    def welcome_banner(context:)
+    def build_banner
       Application::PermanentlyDismissableBanner.new(
-        content: { text: I18n.t("site.description_html").html_safe },
         name: WELCOME_BANNER_NAME,
-        context: BannerContext.new(context:))
-    end
-  end
-
-  # Home::Show::BannerContext
-  class BannerContext
-    def initialize(context:)
-      @base_context = context
+        content: { text: },
+        context:)
     end
 
-    def show_banner_dismissal_button?
-      @base_context.current_user_has_signed_their_name?
+    private
+
+    attr_reader :context
+
+    def text = I18n.t("site.description_html").html_safe
+
+    # Home::Show::WelcomeBanner::BannerContext
+    class BannerContext
+      def initialize(context:)
+        @base_context = context
+      end
+
+      def cookies(...) = @base_context.cookies(...)
+
+      def show_banner_dismissal_button?
+        @base_context.current_user_has_signed_their_name?
+      end
     end
   end
 end
