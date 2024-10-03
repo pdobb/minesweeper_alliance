d11a001f28bbeb5c08aef079afb5b1f5f7118fa2
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-10-03 09:31:49 -0500

    Customize tailwind config: letter-spacing for emojis
    Fix a spot where we were missing proper letter spacing for emojis.
    At the same time, I went ahead and added a custom entry for emoji-width
    letter spacing to our tailwind.config.js.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -21,6 +21,9 @@ module.exports = {
       fontFamily: {
         // sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      letterSpacing: {
+        emojis: ".5em",
+      },
     },
   },
   plugins: [
