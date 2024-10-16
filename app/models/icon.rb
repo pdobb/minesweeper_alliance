fac61446059427145f2addd5a0b64b2c4960641c
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-10-16 17:08:32 -0500

    Alphabetize and simplify Icon module

--- app/models/icon.rb
+++ app/models/icon.rb
@@ -1,69 +1,60 @@
 # frozen_string_literal: true
 
-# Icon is a module (as in a Class that is never instantiated) for easily
-# referencing the various icons we use throughout the app.
+# Icon is a registry of the various emojis/icons we use throughout the app.
 module Icon
-  def self.cell
-    "◻️"
-  end
-
-  def self.revealed_cell
-    "☑️"
-  end
-
-  def self.flag
-    "🚩"
-  end
-
-  def self.mine
-    "💣"
-  end
-
-  def self.eyes
-    "👀"
-  end
-
-  def self.ship
-    "⛴️"
-  end
-
   def self.anchor
     "⚓️"
   end
 
-  def self.victory
-    "🎉"
-  end
-
-  def self.clover
-    "🍀"
-  end
-
-  def self.heart
-    "❤️"
+  def self.cell
+    "◻️"
   end
 
   def self.check
     "✅"
   end
 
-  def self.pencil
-    "✏️"
+  def self.clover
+    "🍀"
   end
 
-  def self.trash
-    "🗑️"
+  def self.eyes
+    "👀"
+  end
+
+  def self.flag
+    "🚩"
+  end
+
+  def self.heart
+    "❤️"
+  end
+
+  def self.mine
+    "💣"
+  end
+
+  def self.pencil
+    "✏️"
   end
 
   def self.profile
     "🎖️"
   end
 
-  def self.celebratory_victory
-    "#{ship}#{anchor}#{victory}"
+  def self.revealed_cell
+    "☑️"
   end
 
-  def self.humiliating_defeat
-    mine
+  def self.ship
+    "⛴️"
+  end
+
+  def self.trash
+    "🗑️"
+  end
+
+  def self.victory
+    "🎉"
   end
 end
--- app/views/games/status_behaviors.rb
+++ app/views/games/status_behaviors.rb
@@ -3,11 +3,13 @@
 # Games::StatusBehaviors defines common code needed by the Games::* View models
 # for showing Game completion status.
 module Games::StatusBehaviors
+  # :reek:DuplicateMethodCall
+
   def game_status_mojis
     if game_ended_in_defeat?
-      Icon.humiliating_defeat
+      Icon.mine
     elsif game_ended_in_victory?
-      Icon.celebratory_victory
+      "#{Icon.ship}#{Icon.anchor}#{Icon.victory}"
     elsif game_in_progress?
       Icon.ship
     else # Standing By
