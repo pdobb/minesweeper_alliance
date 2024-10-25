5c16ae1b21d056ee5bd6459d8377e4317d39e979
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-10-25 17:05:28 -0500

    Fix up HTML issues found by w3c validator
    I'm trying to debug an odd issue, so I ran the major pages of the site
    through the w3c validator. It didn't help my issue, but it's still
    worth fixing these HTML spec violations up.

--- app/views/games/rules.rb
+++ app/views/games/rules.rb
@@ -13,6 +13,9 @@ class Games::Rules
 
   def cookie_name = COOKIE_NAME
 
+  def button_id = @button_id ||= "rules_button-#{Time.new.to_i}"
+  def content_id = @content_id ||= "rules_content-#{Time.new.to_i}"
+
   def button_text = "Rules of Engagement"
 
   def button_css_class
