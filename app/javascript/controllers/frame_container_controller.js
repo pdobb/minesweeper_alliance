8ce220fdca8aefd5c02f2ae5e7a4058c54870df3
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-11-03 18:13:12 -0600

    Rework sub-framed Game + show + scroll structure/behaviors
    - DRY up the turbo_frame_tag + container into a new
      "games/frame_container" partial. The code for both of these was
      previously identical, and I don't foresee them needing to diverge.
    - Rename frame_controller.js -> frame_container_controller.js. I got
      quickly confused by the previous name, as it blends in easily with
      the concept of `turbo_frame_tag`-style frame.
    - Improve frame_container_controller.js' state tracking so we can
      reliably react to the turbo_frame_tag's "load" events. Turbo combines
      several distinct events into one generic "load" event:
        - empty -> new content
        - old content -> new content
        - old content -> empty
      - This breakdown of events fixes little issue with
        showing/hiding/scrolling to the clicked-on Game for User Show and
        Metrics Show pages.

---
+++ app/javascript/controllers/frame_container_controller.js
@@ -0,0 +1,65 @@
+import { Controller } from "@hotwired/stimulus"
+import { enter, leave } from "el-transition"
+
+// FrameContainerController represents a dynamically shown/hideable view frame.
+export default class extends Controller {
+  static targets = ["turboFrame"]
+
+  static SHOWING_STATE = "showing"
+  static VISIBLE_STATE = "visible"
+  static HIDING_STATE = "hiding"
+  static EMPTY_STATE = "empty"
+
+  load() {
+    if (this.#isEmpty()) {
+      // Loading new content.
+      this.show()
+    } else if (this.#isVisible()) {
+      // Just changing source content.
+      this.dispatch("show")
+    } else {
+      // Hiding
+    }
+  }
+
+  show() {
+    this.#setState(this.constructor.SHOWING_STATE)
+
+    enter(this.element).then(() => {
+      this.#setState(this.constructor.VISIBLE_STATE)
+      this.dispatch("show")
+    })
+  }
+
+  hide(event) {
+    this.#setState(this.constructor.HIDING_STATE)
+
+    event.currentTarget.blur()
+
+    leave(this.element).then(() => {
+      this.#empty()
+      this.#setState(this.constructor.EMPTY_STATE)
+      this.dispatch("hide")
+    })
+  }
+
+  #empty() {
+    this.turboFrameTarget.replaceChildren()
+  }
+
+  #setState(value) {
+    this.turboFrameTarget.dataset.state = value
+  }
+
+  #isVisible() {
+    return this.#getState() == this.constructor.VISIBLE_STATE
+  }
+
+  #isEmpty() {
+    return this.#getState() == this.constructor.EMPTY_STATE
+  }
+
+  #getState() {
+    return this.turboFrameTarget.dataset.state
+  }
+}
