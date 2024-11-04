cc75af4a641e3ce4e332e31a61402846e5625124
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-11-03 22:15:49 -0600

    Only scroll down to Game "frame" if needed/helpful
    Always scrolling, even when you can already see the Game "frame" well
    enough is a hindrance. This function is mostly useful when the Game
    "frame" is all or mostly off screen. We can use the bounding box of the
    "frame", then, to determine if we should scroll or not.
    - We also add a "threshold" value to determine how much of the element
      can protrude into the viewport before considering it to be "on-screen
      enough" to warrant scrolling. Default is -200px.

--- app/javascript/controllers/scroll_to_controller.js
+++ app/javascript/controllers/scroll_to_controller.js
@@ -5,16 +5,18 @@ export default class extends Controller {
     offset: { type: Number, default: 50 },
     delay: { type: Number, default: 200 },
     behavior: { type: String, default: "smooth" },
+    threshold: { type: Number, default: -200 },
   }
 
   jumpToContainer() {
     this.#doScroll(this.element)
   }
 
-  #doScroll(target) {
-    const elementPosition =
-      target.getBoundingClientRect().top + window.pageYOffset
-    const offsetPosition = elementPosition - this.offsetValue
+  #doScroll($target) {
+    const scrollTarget = new ScrollTarget($target)
+    if (scrollTarget.isOnScreen({ threshold: this.thresholdValue })) return
+
+    const offsetPosition = scrollTarget.scrollPosition - this.offsetValue
 
     setTimeout(() => {
       window.scrollTo({
@@ -22,5 +24,25 @@ export default class extends Controller {
         behavior: this.behaviorValue,
       })
     }, this.delayValue)
+  }
+}
+
+class ScrollTarget {
+  constructor(element) {
+    this.element = element
+  }
+
+  get bounds() {
+    return this.element.getBoundingClientRect()
+  }
+
+  get scrollPosition() {
+    return this.bounds.top + window.pageYOffset
+  }
+
+  isOnScreen({ threshold = 0 } = {}) {
+    const viewportHeight =
+      window.innerHeight || document.documentElement.clientHeight
+    return this.bounds.top < viewportHeight + threshold
   }
 }
