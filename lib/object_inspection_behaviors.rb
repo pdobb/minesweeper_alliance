667ef192145cf1e3c8f8db3eb473cafec0e7b4b8
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2024-11-22 09:20:08 -0600

    Add #original_inspect to ObjectInspectionBehaviors
    This allows us to maintain access to e.g. Rails' #inspect method via
    the #original_inspect alias.

--- lib/object_inspection_behaviors.rb
+++ lib/object_inspection_behaviors.rb
@@ -4,8 +4,14 @@
 # the object_inspector gem. Using this approach, we are able to e.g.
 # conditionally enable object_inspector throughout the entire app.
 module ObjectInspectionBehaviors
-  unless App.debug? # rubocop:disable Style/IfUnlessModifier
-    include ObjectInspector::InspectorsHelper
+  extend ActiveSupport::Concern
+
+  included do
+    alias_method :original_inspect, :inspect
+
+    unless App.debug? # rubocop:disable Style/IfUnlessModifier
+      include ObjectInspector::InspectorsHelper
+    end
   end
 
   def introspect
