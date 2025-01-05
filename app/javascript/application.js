45febf3b589aa9d2e688cccded170ab4c0473658
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-01-04 19:50:34 -0600

    Only update Game board for versions > current version
    We used to use Time.now.to_i to determine the current version of the
    board being broadcasted. This was needed for cases where multiple
    updates occurred within a second. But it doesn't make sense now since
    we're using Time.now.to_f to determine the current version. This gives
    us micro-second-level accuracy. So using `>=` is now only causing us to
    morph the page for actually equivalent Game board updates.
    
    I think this is the cause of some wonky update behaviors I'm seeing.
    However, I'm really only seeing them on production, so it's been hard to
    diagnose. Either way, even if this isn't it, I think it's still the
    right change to make regardless.
    
    NOTE: To make this work, I also had to fix an oversight in the JS
    function: Turbo.StreamActions.versioned_replace. Previously we were
    doing parseInt instead of parseFloat, which has been needed ever since
    the commit titled "Attempt to fix out-of-sequence Game updates", with
    hash: b14242da53e68bd7557b9feabb07f6181a7c6ee1

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,10 +3,10 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 Turbo.StreamActions.versioned_replace = function () {
-  const currentVersion = parseInt(this.targetElements[0].dataset.version)
-  const payloadVersion = parseInt(
+  const currentVersion = parseFloat(this.targetElements[0].dataset.version)
+  const payloadVersion = parseFloat(
     this.templateContent.children[0].dataset.version,
   )
 
-  if (payloadVersion >= currentVersion) Turbo.StreamActions.replace.call(this)
+  if (payloadVersion > currentVersion) Turbo.StreamActions.replace.call(this)
 }
