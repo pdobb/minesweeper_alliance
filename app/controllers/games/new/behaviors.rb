27e08e5452080efa8267cca68a15fb1aafadbf81
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-01-14 11:43:56 -0600

    Refactor Games::New::Behaviors to use new OnCreate service obj
    Functionality remains the same, but this is easier to think about since
    it breaks out responsibilities, reduces param passing, encapsulates
    the constant, and reduces responsibility / cognitive load for the
    find_or_create_current_game method's `tap {}` call.

--- app/controllers/games/new/behaviors.rb
+++ app/controllers/games/new/behaviors.rb
@@ -2,34 +2,53 @@
 
 # Games::New::Behaviors defines common controller code for {Game} creation.
 module Games::New::Behaviors
-  TURBO_STREAM_DISCONNECT_AFFORDANCE_IN_SECONDS = 0.25.seconds
-
   def find_or_create_current_game(settings:)
     Game.
       find_or_create_current(settings:, user: current_user).
-      tap { |current_game|
-        if current_game.just_created?
-          FleetTracker.reset
-          WarRoomChannel.broadcast_refresh
-          broadcast_new_game_notification(
-            wait: TURBO_STREAM_DISCONNECT_AFFORDANCE_IN_SECONDS)
-
-          store_board_settings(current_game)
-        end
-      }
+      tap { |current_game| OnCreate.(current_game:, context: layout) }
   end
 
-  private
+  # Games::New::Behaviors::OnCreate
+  class OnCreate
+    TURBO_STREAM_DISCONNECT_AFFORDANCE_IN_SECONDS = 0.25.seconds
 
-  def broadcast_new_game_notification(wait:)
-    Games::BroadcastCreateNotificationJob.set(wait:).perform_later
-  end
+    include CallMethodBehaviors
 
-  def store_board_settings(current_game)
-    return unless (settings = current_game.board_settings).custom?
+    def initialize(current_game:, context:)
+      @current_game = current_game
+      @context = context
+    end
 
-    layout.store_http_cookie(
-      Games::New::Custom::Form::STORAGE_KEY,
-      value: settings.to_json)
+    def call
+      return unless just_created?
+
+      FleetTracker.reset
+      WarRoomChannel.broadcast_refresh
+      broadcast_new_game_notification
+      store_board_settings
+    end
+
+    private
+
+    attr_reader :current_game,
+                :context
+
+    def just_created? = current_game.just_created?
+
+    def broadcast_new_game_notification(
+          wait: TURBO_STREAM_DISCONNECT_AFFORDANCE_IN_SECONDS)
+      Games::BroadcastCreateNotificationJob.set(wait:).perform_later
+    end
+
+    def store_board_settings
+      return unless custom_settings?
+
+      context.store_http_cookie(
+        Games::New::Custom::Form::STORAGE_KEY,
+        value: settings.to_json)
+    end
+
+    def settings = @settings ||= current_game.board_settings
+    def custom_settings? = settings.custom?
   end
 end
