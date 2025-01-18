878dd04e1ee3d12c6a25c81fc677ff02c7bdc34a
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-01-18 11:43:45 -0600

    Rescue on error when update to User's Time zone fails
    Still seeing the `Etc/Unknown` Time Zone string coming from our JS
    script in production. This causes Rails to choke when attempting to set
    `Time.zone` for the current User session. Since we really can't help
    this any better way than defaulting to the application's Time Zone, we
    just swallow the error and move on. There may be a way to detect if the
    Time Zone is valid instead of using error handling to deal with this,
    but I'm OK with this approach since it is a non-standard flow / somewhat
    exceptional situation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,5 +20,11 @@ class ApplicationController < ActionController::Base
     if (time_zone = current_user.time_zone).present?
       Time.zone = time_zone
     end
+  rescue ArgumentError
+    # We use JavaScript to discover the user's current Time Zone. However,
+    # sometimes this resolves to `Etc/Unknown`, perhaps from e.g. bots/crawlers
+    # in a headless environment. Either way, when Rails is given this it raises:
+    # `ArgumentError: Invalid Timezone: Etc/Unknown`. We're fine to just swallow
+    # this error type and move on with the default Time zone.
   end
 end
