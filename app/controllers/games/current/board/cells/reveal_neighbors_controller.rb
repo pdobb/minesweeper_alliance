6d17821816146404c9fe98e4348a2e7e360b8194
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-01-20 23:35:03 -0600

    Refactor: Move Cell dehighlighting logic up the stack
    This is in prep for extracting Cell dehighlighting into a service object
    like what Cell highlighting has.
    
    First make the change easy, then make the easy change.
    - Fortunately, this wasn't that hard.
    - And, actually, this is where the conditional should have always been.
      I keep finding that conditionals--which should always be pushed up
      the stack--tend to end up landing in Controllers. 😎

--- app/controllers/games/current/board/cells/reveal_neighbors_controller.rb
+++ app/controllers/games/current/board/cells/reveal_neighbors_controller.rb
@@ -6,7 +6,17 @@ class Games::Current::Board::Cells::RevealNeighborsController <
 
   def create
     safe_perform_game_action do
-      Cell::RevealNeighbors.(current_context)
+      # Whether we end up revealing any neighboring {Cell}s or not, we will
+      # always need to dehighlight any highlighted neighboring {Cell}s as per
+      # our game play rules.
+      # - If neighbors are revealed, {Cell#reveal} takes care of the
+      #   de-highlighting of those {Cell}s for us.
+      # - Else, we must take care of this here ourselves.
+      if cell.neighboring_flags_count_matches_value?
+        Cell::RevealNeighbors.(current_context)
+      else
+        cell.dehighlight_neighbors
+      end
 
       render_updated_game
     end
--- app/models/cell/reveal_neighbors.rb
+++ app/models/cell/reveal_neighbors.rb
@@ -2,26 +2,19 @@
 
 # Cell::RevealNeighbors is a Service Object for collaborating with a {Game},
 # {Board}, and {Cell} to process a {Cell} "Reveal Neighbors" event (a.k.a.
-# "chording"). In order for processing to proceed:
+# "chording"). Prerequisites for processing:
 # - The given, originating {Cell} must have been previously revealed, and
 # - The number of flagged neighboring {Cell}s must match the given {Cell}'s
 #   {Cell#value}.
 #
-# This service is a convenience. We're saving the player time by just going
-# ahead and revealing all of the non-flagged (and non-revealed) neighboring
-# cells of the given, originating {Cell}. Also, if doing so reveals a Blank
-# {Cell}, then we'll recursively reveal its neighboring {Cell}s, as per usual.
-#
-# Whether we end up revealing any neighboring {Cell}s or not, we will also
-# always need to dehighlight any highlighted, neighboring {Cell}s as per our
-# game play rules.
-# - We take care of this here, in case we don't end up revealing any
-#   neighbors.
-# - If neighbors are revealed, however, {Cell#reveal} takes care of the
-#   de-highlighting of those {Cell}s for us.
+# In the grand scheme this service is basically just a convenience. i.e. We're
+# saving the player time by just going ahead and revealing all of the
+# non-flagged (and non-revealed) neighboring {Cell}s of the given, originating
+# {Cell}. Also, if doing so reveals a Blank {Cell}, then we'll recursively
+# reveal its neighboring {Cell}s, as per usual.
 #
 # Notes:
-# - If this operation results in a win or loss, we need to capture that.
+# - If this operation results in a Game Win or Loss, we need to capture that.
 # - Revealing neighbors doesn't guarantee success. If any of the flags are
 #   incorrectly placed a mine could still go off!
 #
@@ -45,7 +38,7 @@ class Cell::RevealNeighbors
     return self if cell.unrevealed?
 
     catch(:return) {
-      reveal_or_dehighlight_neighbors
+      reveal_neighbors
       end_game_in_victory_if_all_safe_cells_revealed
 
       self
@@ -53,14 +46,6 @@ class Cell::RevealNeighbors
   end
 
   private
-
-  def reveal_or_dehighlight_neighbors
-    if cell.neighboring_flags_count_matches_value?
-      reveal_neighbors
-    else
-      cell.dehighlight_neighbors
-    end
-  end
 
   def reveal_neighbors
     return if revealable_neighboring_cells.none?
