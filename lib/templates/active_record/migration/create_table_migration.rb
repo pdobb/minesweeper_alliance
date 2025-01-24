10b2b495220b96a180aec39d0b969a2f2258a8f2
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-01-24 17:32:01 -0600

    Update style in Migration generator templates
    Prefer parens. Because they're less work when refactoring for line
    length.

--- lib/templates/active_record/migration/create_table_migration.rb
+++ lib/templates/active_record/migration/create_table_migration.rb
@@ -3,16 +3,16 @@
 # Version: <%= migration_number %>
 class <%= migration_class_name %> < ActiveRecord::Migration[<%= ActiveRecord::Migration.current_version %>]
   def change
-    create_table :<%= table_name %><%= primary_key_type %> do |t|
+    create_table(:<%= table_name %><%= primary_key_type %>) do |t|
 <% attributes.each do |attribute| -%>
 <% if attribute.password_digest? -%>
-      t.string :password_digest<%= attribute.inject_options %>
+      t.string(:password_digest<%= attribute.inject_options %>)
 <% elsif attribute.token? -%>
-      t.string :<%= attribute.name %><%= attribute.inject_options %>
+      t.string(:<%= attribute.name %><%= attribute.inject_options %>)
 <% elsif attribute.reference? -%>
-      t.<%= attribute.type %> :<%= attribute.name %><%= attribute.inject_options %><%= foreign_key_type %>
+      t.<%= attribute.type %>(:<%= attribute.name %><%= attribute.inject_options %><%= foreign_key_type %>)
 <% elsif !attribute.virtual? -%>
-      t.<%= attribute.type %> :<%= attribute.name %><%= attribute.inject_options %>
+      t.<%= attribute.type %>(:<%= attribute.name %><%= attribute.inject_options %>)
 <% end -%>
 <% end -%>
 <% unless attributes.empty? -%>
@@ -23,10 +23,10 @@ class <%= migration_class_name %> < ActiveRecord::Migration[<%= ActiveRecord::Mi
 <% end -%>
     end
 <% attributes.select(&:token?).each do |attribute| -%>
-    add_index :<%= table_name %>, :<%= attribute.index_name %><%= attribute.inject_index_options %>, unique: true
+    add_index(:<%= table_name %>, :<%= attribute.index_name %><%= attribute.inject_index_options %>, unique: true)
 <% end -%>
 <% attributes_with_index.each do |attribute| -%>
-    add_index :<%= table_name %>, :<%= attribute.index_name %><%= attribute.inject_index_options %>
+    add_index(:<%= table_name %>, :<%= attribute.index_name %><%= attribute.inject_index_options %>)
 <% end -%>
   end
 end
