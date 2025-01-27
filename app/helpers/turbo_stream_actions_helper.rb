5c5bbbefe47bf9a448e1c31497603fc7cb2dae78
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-01-27 14:59:05 -0600

    Turn off Turbo prefetch for most links
    Instead of zeroing in on each link or link type to not pre-fetch, this
    commit just disabled it for entire pages.
    
    See: https://turbo.hotwired.dev/handbook/drive#prefetching-links-on-hover
    
    I'm making this change, not because I've seen and measured that we're
    seeing too much activity on the site. But, just out of a gut feel that
    it's not worth the extra requests when hovering over links. We've
    already got so many requests coming in just for the Game play itself!
    So I'm just proactively making this change. Besides, the pages load so
    fast on their own that the pre-fetch doesn't seem to add much of
    anything.

---
+++ app/helpers/turbo_stream_actions_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# TurboStreamActionsHelper defines our custom turbo-rails actions.
+#
+# Notes:
+# - `@view_context` comes from Turbo::Streams::TagBuilder in the turbo-rails
+#   gem. See: https://github.com/hotwired/turbo-rails/blob/0ea92f344395234dd7fc39b0ef9ab02388e33235/app/models/turbo/streams/tag_builder.rb#L47
+module TurboStreamActionsHelper
+  # rubocop:disable Rails/HelperInstanceVariable
+  def sourced_replace_for(view_model, method: :morph)
+    tag.turbo_stream(
+      tag.template(@view_context.render(partial: view_model, formats: :html)),
+      action: :replace,
+      target: view_model.dom_id,
+      method:,
+      data: { source: @view_context.cookies[CurrentUser::COOKIE] })
+  end
+  # rubocop:enable Rails/HelperInstanceVariable
+end
+
+Turbo::Streams::TagBuilder.prepend(TurboStreamActionsHelper)
