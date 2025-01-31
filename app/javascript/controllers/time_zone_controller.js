a73ab95b76b13c41464952e78074d404519bfe82
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-01-31 09:05:48 -0600

    Tidy: Extract/use window.USER_COOKIE from JS code
    Just trying to make the code shier while also using our new convention
    of defining JS cookie names as constants in application.js.
    
    This will be helpful in the future.

--- app/javascript/controllers/time_zone_controller.js
+++ app/javascript/controllers/time_zone_controller.js
@@ -2,16 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 import { post } from "@rails/request.js"
 import { cookies } from "cookies"
 
-// Connects to data-controller="timezone"
+// TimeZoneController is responsible for:
+// 1. Automatically storing the Current {User}'s local Time Zone, as determined
+//    by the browser + JavaScript. Note, however, that we must check if a
+//    Current {User} exists first.
+// 2. Saving the Current {User}'s self-selected Time Zone.
 export default class extends Controller {
   static values = { updateUrl: String }
 
   connect() {
-    if (cookies.isBlank(window.TIME_ZONE_COOKIE)) this.#setCurrentUserTimeZone()
+    if (this.#shouldSetCurrentUserTimeZone()) this.#setCurrentUserTimeZone()
   }
 
   select(event) {
     this.#submit(event.currentTarget.value)
+  }
+
+  #shouldSetCurrentUserTimeZone() {
+    return this.#timeZoneCookieDoesNotExist() && this.#userTokenCookieExists()
+  }
+
+  #timeZoneCookieDoesNotExist() {
+    return cookies.isBlank(window.TIME_ZONE_COOKIE)
+  }
+
+  #userTokenCookieExists() {
+    return cookies.isPresent(window.USER_COOKIE)
   }
 
   #setCurrentUserTimeZone() {
