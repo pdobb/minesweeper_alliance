abe29fb1298d3252703ff906bbc93f040dc8a4ed
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-02-05 19:05:36 -0600

    Remove fleetMusteringNotificationContainer i/f/o flash
    We don't need a specialized notification container for displaying the
    "Fleet Mustering!" message at Game end. We can just use the normal
    Flash Notifications container.
    - I can't fully recall why I thought a separate container was necessary,
      but I think it had something to do with not fully understanding the
      structure of the Flash view code (which we just tidied up in the
      previous commit).
    
    Also, "prepend" the notification instead of "update"ing the flash
    container. This way we can get progressive instances of these messages.
    - At the same time, we let the 10-second timer play out for these
      notifications now.

--- app/jobs/game/current/broadcast_fleet_mustering_notification_job.rb
+++ app/jobs/game/current/broadcast_fleet_mustering_notification_job.rb
@@ -4,11 +4,11 @@ class Game::Current::BroadcastFleetMusteringNotificationJob < ApplicationJob
   queue_as :default
 
   def perform
-    Turbo::StreamsChannel.broadcast_update_to(
+    Turbo::StreamsChannel.broadcast_prepend_to(
       Games::JustEnded::ActiveParticipants::Footer.turbo_stream_name,
-      target: "fleetMusteringNotificationContainer",
-      partial: "layouts/flash/notifications",
-      locals: { notifications: notification })
+      target: Application::Flash.turbo_target,
+      partial: "application/flash/notification",
+      locals: { notification: })
   end
 
   private
@@ -16,9 +16,6 @@ class Game::Current::BroadcastFleetMusteringNotificationJob < ApplicationJob
   def notification
     Application::Flash::Notification.new(
       type: :info,
-      content: {
-        text: I18n.t("flash.fleet_mustering_notification_html").html_safe,
-        timeout: false,
-      })
+      content: I18n.t("flash.fleet_mustering_notification_html").html_safe)
   end
 end
