d8063f05c4d1e524f2a961a03c73d8f516ede7d0
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-02-09 12:03:26 -0600

    Dim the text on "no value indicators"
    Now we make the easy change in User Show pages, to dim the "no value
    indicators", now that we've refactored User -> Bests -> Metrics in the
    previous commit.
    
    This is done to match the style from the Metrics page, which I noticed
    wasn't carried over for User Bests (because it was too complex to do
    before, for what it was).
    
    Also dim the indicator in the Home Roster when no non-Guest Users are
    present.

--- app/views/users/bests/metrics/behaviors.rb
+++ app/views/users/bests/metrics/behaviors.rb
@@ -4,11 +4,25 @@
 
 # Users::Bests::Metrics::Behaviors
 module Users::Bests::Metrics::Behaviors
+  NO_VALUE_INDICATOR_CONTAIENR_CSS = "text-dim-lg"
+
   include Games::Past::ActiveLinkBehaviors
 
   def initialize(user:, user_bests:)
     @user = user
     @user_bests = user_bests
+  end
+
+  def beginner_table_cell_css
+    NO_VALUE_INDICATOR_CONTAIENR_CSS unless beginner_value?
+  end
+
+  def intermediate_table_cell_css
+    NO_VALUE_INDICATOR_CONTAIENR_CSS unless intermediate_value?
+  end
+
+  def expert_table_cell_css
+    NO_VALUE_INDICATOR_CONTAIENR_CSS unless expert_value?
   end
 
   private
