9e21b29f558feb17923bb860cae5b06cbaed49ac
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-02-23 00:09:58 -0600

    Fix up styling of flash notifications
    Fix centering of flash notifications to not have an awkward amount
    of space around them by removing the `left-1/2 -translate-x-1/2` and
    adding, instead, `inset-x-0 px-12`. This stretches the container across
    the page while leaving an intentional margin buffer on either side. (The
    net result is less of a margin buffer and allowing the notification
    container to stretch a lot more on wider browser widths--on desktop.)
    
    Also, move the flash notifications container down a bit so it leaves
    more of the main nav bar exposed. This feels a bit better for the
    overall design on this site.
    
    Finally: Make the sample flash messages use our actual flash messages,
    just with dummy text interpolated in.

--- app/controllers/ui_portal/flash_notifications_controller.rb
+++ app/controllers/ui_portal/flash_notifications_controller.rb
@@ -1,20 +1,42 @@
 # frozen_string_literal: true
 
 class UIPortal::FlashNotificationsController < UIPortal::BaseController
+  # @example /ui/flash_notifications
+  # @example /ui/flash_notifications?count=3&timeout=1
   def show
-    now = l(Time.current, format: :debug)
-
-    types = Application::Flash.notification_types
-    types = types.sample(params[:count].to_i) if params.key?(:count)
-
-    types.each do |type|
-      flash.now[type] = [
-        "Test #{type} #{now}",
-        {
-          text: "Test #{type} w/ no Auto-Timeout and w/ XL content. #{now}",
-          timeout: false,
-        },
-      ]
+    sample_messages.first(count).each do |message|
+      type = Application::Flash.notification_types.sample
+      notifications = (flash.now[type] ||= [])
+      notifications << {
+        text: message.html_safe, # rubocop:disable Rails/OutputSafety
+        timeout:,
+      }
     end
+  end
+
+  private
+
+  def sample_messages
+    t("flash").flat_map { |_, value|
+      Array.wrap(value).map { I18n.interpolate(it, dummy_values) }
+    }.shuffle!
+  end
+
+  def dummy_values
+    @dummy_values ||= {
+      url: "/ui/flash_notifications",
+      type: "[Example Type]",
+      category: "[Example Category]",
+      to: "[New Value]",
+      name: "[Example Name]",
+    }
+  end
+
+  def count(default: 8)
+    (params[:count] || default).to_i
+  end
+
+  def timeout(default: 10)
+    params.key?(:timeout) ? params[:timeout].to_i : [nil, default].sample
   end
 end
