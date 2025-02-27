106fe90837e206681a98829c575046600509a998
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-02-26 22:33:24 -0600

    Implement per-message deflate (as an experiment)
    See:
    - https://sapandiwakar.in/compressing-websocket-actioncable-frames-on-rails-with-permessage-deflate/
    - https://github.com/faye/permessage-deflate-ruby
    
    I can't detect that this is doing anything in development, so going to
    try it out in production.

---
+++ config/initializers/action_cable.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# :reek:InstanceVariableAssumption
+
+# Monkey-patch ActionCable::Connection::ClientSocket to use permessage-deflate.
+# See:
+# - https://github.com/faye/permessage-deflate-ruby
+# - https://sapandiwakar.in/compressing-websocket-actioncable-frames-on-rails-with-permessage-deflate/
+class ActionCable::Connection::ClientSocket
+  alias_method :old_initialize, :initialize
+
+  def initialize(...)
+    old_initialize(...)
+    @driver.add_extension(PermessageDeflate)
+  end
+end
