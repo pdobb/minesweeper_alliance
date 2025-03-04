86453ddb76d23e4dad97b85794af1c827b7bfd4f
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-03-04 15:42:23 -0600

    Make Home page - New Game title stay same for 30s at a time
    While I've chosen 30s as a happy medium for caching, the primary goal
    here is to make the message sync across all players/refreshes. So they
    get the same, but otherwise randomized, message to start.
    
    This follows in the footsteps of our previous commit which did the
    same for the just-ended Game message.

--- app/views/games/new/container.rb
+++ app/views/games/new/container.rb
@@ -3,9 +3,16 @@
 # Games::New::Container represents the entire view context surrounding new
 # {Game}s, as a partial for reuse.
 class Games::New::Container
+  TITLE_CACHE_EXPIRES_DURATION = 30.seconds
+
   def partial_path
     "games/new/container"
   end
+
+  def title_cache_key = :new_game_title
+  def title_cahce_expires_in = TITLE_CACHE_EXPIRES_DURATION
+
+  def title = I18n.t("game.new_html").sample
 
   def content
     Games::New::Content.new
