e0099033ae15d6d9bf6e825873511fbdbf5c514e
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2025-05-09 00:18:37 -0500

    Update gems/packages
    Nothing specific, just routine updates.

--- config/importmap.rb
+++ config/importmap.rb
@@ -21,7 +21,7 @@ pin "touch", to: "lib/touch.js"
 # External libraries
 
 pin "el-transition" # @0.0.7
-pin "@floating-ui/dom", to: "@floating-ui--dom.js" # @1.6.13
-pin "@floating-ui/core", to: "@floating-ui--core.js" # @1.6.9
+pin "@floating-ui/dom", to: "@floating-ui--dom.js" # @1.7.0
+pin "@floating-ui/core", to: "@floating-ui--core.js" # @1.7.0
 pin "@floating-ui/utils", to: "@floating-ui--utils.js" # @0.2.9
 pin "@floating-ui/utils/dom", to: "@floating-ui--utils--dom.js" # @0.2.9
